985e79661433b2f5d1e1fafe542cd685b34ec639
Tommy Goossens <tommygoossens@ziggo.nl>
2020-04-20 15:42:08 +0200

    Disabled the browser launch

--- OcelotGateway/Startup.cs
+++ OcelotGateway/Startup.cs
@@ -1,16 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authentication.Certificate;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
@@ -20,7 +12,7 @@ namespace OcelotGateway
     {
         public Startup(IHostEnvironment env)
         {
-            var builder = new Microsoft.Extensions.Configuration.ConfigurationBuilder();  
+            var builder = new ConfigurationBuilder();  
             builder.SetBasePath(env.ContentRootPath)      
                 //add configuration.json  
                 .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)  
